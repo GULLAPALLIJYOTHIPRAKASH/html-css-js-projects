a4c4c023e68b11100ac70b416bed15f3d9439ee3
GULLAPALLI JYOTHI PRAKASH <gsraolic@gmail.com>
2023-07-14 20:07:27 +0530

    "first commit"

---
+++ digital-clock/js/app.js
@@ -0,0 +1,70 @@
+// add  timer to call function digital_clock at every 1 second;
+
+  setInterval(digital_clock,1000);
+
+
+//   function for digital_clock
+
+
+    function  digital_clock() {
+
+
+
+    // hours,minutes,seconds, time_indicator(AM/PM);
+
+    let hours=document.getElementById("hours");
+
+    let minutes=document.getElementById("minutes");
+
+    let seconds=document.getElementById("seconds");
+
+    let time_indicator=document.getElementById("time");
+
+    // console.log(hours,minutes,seconds,time_indicator);
+
+    let date=new Date();
+
+    // new array for 24 hours
+
+    let hours_24=[0,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12];
+
+    // converted  24 hours  into 12 hours  and  to check the values hour is < 10 then add 0 hour like  01 ,02,03
+
+    let d_hours=hours_24[date.getHours()]  < 10  ? `0${hours_24[date.getHours()] }` : `${hours_24[date.getHours()]}` ;
+
+    // console.log(d_hours);
+
+    // to check the values minutes is < 10 then add 0 minutes  like  01 01 ,02,03
+    
+    let  d_minutes=date.getMinutes() < 10 ? `0${date.getMinutes()}` : `${date.getMinutes()}`;
+
+
+    // console.log(d_minutes);
+
+    // to check the values seconds is < 10 then add 0 seconds  like  01 01 ,02,03
+
+    let d_seconds=date.getSeconds() < 10 ? `0${date.getSeconds()}` :  `${date.getSeconds()}`;
+
+    // console.log(d_seconds);
+
+    
+    // to get the AM or PM when time is <= 11
+    
+    let d_time_indicator= hours_24[date.getHours()]  >= 11 ? 'AM' : 'PM';
+    
+    // console.log(d_time_indicator);
+
+
+    // add to hours,minutes,seconds and AM/PM to html elements
+
+    hours.innerText=  `${d_hours}`;
+
+    minutes.innerText= `${d_minutes}`;
+
+    seconds.innerText= `${d_seconds}`;
+
+    time_indicator.innerText= `${d_time_indicator}`;
+}
+
+
+
