212082d2aea05de3b13b8d2c5ce6f0efc8d77130
GULLAPALLI JYOTHI PRAKASH <gsraolic@gmail.com>
2023-09-05 15:28:10 +0530

    "stopwatch with localstorage"

--- stopwatch/js/app.js
+++ stopwatch/js/app.js
@@ -1,7 +1,6 @@
 
 
 // stopWatch function 
-
 function stopWatch(){
 
     // we get the stopwatch timer div to display the timer
@@ -14,15 +13,69 @@ function stopWatch(){
     let pause=document.getElementById("pause");
     let reset=document.getElementById("reset");
 
-    // console.log(start,pause,reset);
+    
+
+    // calling this function to  execute local_storage()   and  time.innertext with localstorage variable values
+    check_storage();
+ 
+
+    
+    // declared variable with localstorage item is zero;
+    let hour =Number( localStorage.getItem('h'));
+    let  minute =  Number( localStorage.getItem('m'));
+    let second = Number(localStorage.getItem('s'));
+    let milliSecond =Number( localStorage.getItem('ms'));
 
 
-    // declared variable with zero;
+    // check_storage
+   function check_storage(){
 
-    let hour = 0 ;
-    let minute = 0 ;
-    let second = 0 ;
-    let milliSecond =0;
+    // initail localstorage is empty then execute local_storage()
+    if(localStorage.length === 0){
+
+        local_storage();
+
+
+    }
+
+    // also update innertext of timer with localstorage variable values
+    timer.innerText=`${(localStorage.getItem('h'))} : ${(localStorage.getItem('m'))}  : ${(localStorage.getItem('s'))}  : ${(localStorage.getItem('ms'))} `;
+
+   }
+
+
+
+
+    //    local_storage with defualt paraments
+    function local_storage(h="00",m="00",s="00",ms="000") {
+
+ 
+        //  initail localstorage is not set with ms  going inside if only 
+         if (!localStorage.getItem("ms")){
+
+            // initial now we set localstorage with variable value
+            localStorage.setItem("h",h);
+            localStorage.setItem("m",m);
+            localStorage.setItem("s",s);
+            localStorage.setItem("ms",ms);
+
+     
+        }
+        
+        //  we update localstorage with variable value from other function like start,pause,reset
+        localStorage.setItem("h",h);
+        localStorage.setItem("m",m);
+        localStorage.setItem("s",s);
+        localStorage.setItem("ms",ms);
+
+               
+
+                
+
+    
+
+}
+
 
     // to store the values of hour,minute,second, milliSecond
     let h,m,s,ms;
@@ -31,29 +84,29 @@ function stopWatch(){
     let timeref;
 
 
-    start.addEventListener("click",function(){
+    start.addEventListener("click",function(e){
 
         timeref=setInterval(startTimer,10);
+
         });
     
 
     function startTimer(){
 
-
             /* To make user  free to use the stopWatch
-            when timer start visible the pause,
-            reset button then and
+            when timer start  the pause,
+            reset button will visible then and
             hidden start button          */
 
         pause.style.display="inline";
 
         reset.style.display="inline";
-        
+
         start.style.display="none";
 
         milliSecond = milliSecond + 10;
 
-        // console.log(milliSecond);
+       
 
 
         if(milliSecond == 1000){
@@ -66,7 +119,6 @@ function stopWatch(){
                 second = 0;
                 minute = minute + 1;
 
-                // console.log(minute);
 
                 if(minute == 60){
 
@@ -83,16 +135,14 @@ function stopWatch(){
 
         }
 
-    
-             
-
-
-                // to add zero if hour,minute,second,milliSecond is less than 10 mean like 01,02,03...09
+        
+        // to add zero if hour,minute,second,milliSecond is less than 10 mean like 01,02,03...09
 
 
                 if(hour <10){
 
-                    h="0"+hour;
+                    h='0'+hour;
+                    // h=Number('0')+hour;
                 }
                 else{
 
@@ -101,7 +151,8 @@ function stopWatch(){
 
                 if(minute < 10){
 
-                    m="0"+minute;
+                    m='0'+minute;
+                    // m=Number('0')+minute;
 
                 }
 
@@ -113,7 +164,8 @@ function stopWatch(){
 
                 if(second < 10){
 
-                    s="0"+second;
+                    s='0'+second;
+                    // s=Number('0')+second;
                 }
                 else{
 
@@ -122,12 +174,13 @@ function stopWatch(){
 
                 if(milliSecond < 10){
 
-                    ms="00"+milliSecond
+                    ms='00'+milliSecond;
+                    // ms=Number('00')+milliSecond;
                 }
 
                 if(milliSecond < 100){
 
-                    ms="0"+milliSecond;
+                    ms='0'+milliSecond;
                 }
 
                 if(milliSecond >=100){
@@ -135,11 +188,12 @@ function stopWatch(){
                     ms=milliSecond;
                 }
 
-               
-
                 // now h,m,s,ms values into timer innerHTML;
-
+                
                 timer.innerText=`${h} : ${m}  : ${s}  : ${ms} `;
+                
+                // send the value to local_storage function
+                local_storage(h,m,s,ms);
 
         
 
@@ -151,24 +205,27 @@ function stopWatch(){
 
     pause.addEventListener("click",pauseTimer);
 
-    function pauseTimer(){
+    function pauseTimer(e){
 
         clearInterval(timeref);
-        timer.innerText=`${h} : ${m}  : ${s}  : ${ms} `;
 
         
+        timer.innerText=`${h} : ${m}  : ${s}  : ${ms} `;
 
-           
-           // change the inner text of start btn   resume to start
-           start.innerText='play'
+        // send the value to local_storage function
+        local_storage(h,m,s,ms);
 
-                       /* To make user  free to use the stopWatch
-            when user click on pause to make to start  timer 
-            with continues and by visible  start button
-                 
-            */
-           
-           start.style.display="inline";
+
+        /* To make user  free to use the stopWatch 
+        when user click on pause to make to start  timer 
+        with continues and by visible  start button   */                                 
+        
+        pause.style.display="none";
+
+        
+        start.style.display="inline";
+
+    
      }
 
 
@@ -176,46 +233,40 @@ function stopWatch(){
     //  add event to reset button to to clearInterval and  reset the timer with 00:00:00:000
      reset.addEventListener("click",resetTimer);
 
-     function resetTimer(){
+     function resetTimer(e){
 
         clearInterval(timeref);
 
 
+        // reset the values
         hour=0;
         minute=0;
         second=0;
         milliSecond=0;
-        // h=0;
-        // m=0;
-        // s=0;
-        // ms=0;
+        h="00";
+        m="00";
+        s="00";
+        ms="000";
+
+        // send the value to local_storage function
+        local_storage(h,m,s,ms);
 
         timer.innerText=`00 : 00 : 00 : 000`;
 
-
-        /* hidden the pause and reset button 
-         to starting stage and visible the start button 
-         */
-
+    
+        /*  hidden the pause and reset button 
+            to starting stage and visible the start button  */
 
         pause.style.display="none";
 
         reset.style.display="none";
 
-        // change the inner text of start btn   resume to start
-
-        start.innerText="start";
-
         start.style.display="inline";
-
-
-
+   }
 
 
      }
 
-
-    }
 
 
     // calling the stopWatch function
