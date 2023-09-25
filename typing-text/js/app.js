e67302675d3f66c3ce7f585a27060fdc8def0ffc
GULLAPALLI JYOTHI PRAKASH <gsraolic@gmail.com>
2023-09-25 18:11:03 +0530

    "added  with the buttons "

--- typing-text/js/app.js
+++ typing-text/js/app.js
@@ -2,64 +2,86 @@
         function typing_text() {
             
         
-            //  we seleted  text h1 tag, input
+            //  we seleted  text_element and all buttons
             const text_element =document.querySelector(".text");
-            const input=document.querySelector("#speed-counter");
+            let all_btns=document.querySelectorAll("button");
+
+            // count
+           let count=1;
+
+
     
             //  text with content
             const text=`i'am gullapalli jyothi prakash,i pursued my bachelor degree in computer science with cognitive system 2023,i'am  skilled in front web developement  like HTML5, CSS3, Javascript...               `;
-    
-            // speed ,initial speed is 350
-            let speed =350/input.value;
-    
-            // initial count is one
-            let count=1;
-    
-            // added to event when dom laod to call typing_speed function
-            document.addEventListener("DOMContentLoaded", typing_speed);
-    
-            // added to input when value is change
-            input.addEventListener("input", (e) => { speed =350/ e. target.value} );
-    
+
+
+             // speed
+            let speed =350 ;
+
+
+            // to remove active class to buttons 
+            function active_btn_remove(){
+
+                all_btns.forEach((item) => {
+
+                    item.classList.remove("active");   
+
+
+                })
+            }
+
+            all_btns.forEach((item) =>{
+                
+                // added event to all buttons
+                item.addEventListener("click", function(e){
+                    
+                    // remove active class to buttons
+                    active_btn_remove();
+                
+                    // adde to one element on click
+                    e.target.classList.add("active");
+
+                    // speed value 
+                    speed=350/e.target.innerText;
+
+                })
+
+
+            })
+
+            typing_speed();
+
             // function
-            function typing_speed(e){
-    
+            function typing_speed(){
+
                 // slice the text and added to text_element innerText.
                 text_element.innerText=text.slice(0,count);
-    
+                
                 // increment count value;
                 count++;
-    
+                
                 // interval to call the function every time.
                 setTimeout(typing_speed, speed);
-    
-            
+                
+                
+                
                 // count value is greater than text lenght make count=1
                 if(count > text.length){
-                    
-                    count=1;
-                        
+                
+                count=1;
+                
                 }
-    
-    
-    
-                //  if input.value is increased to more than 10 then make input.value to 10
-                if(input.value > 10){
-                    
-                    input.value=10;
-                }
-                //  if input.value is increased to less than 1 then make input.value to 1
-                else if(input.value <1){
-    
-                    input.value=1;
-                }
-                // if  input.value is empty then make input.value to 1;
-                else if(input.value == ""){
-                    
-                    input.value=1;
-                }
-    
+
+
             }
+
+            
+            
+            
+            
+
+            
+    
     
         }
     
